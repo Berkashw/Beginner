8fb2f032ed629ad83db6e40a9564c1af9d734ae4
Berkashw <100141246+Berkashw@users.noreply.github.com>
2022-02-28 18:11:24 +0300

    add files

---
+++ Array/ArrayRemoveUnauthorized.js
@@ -0,0 +1,9 @@
+const person1 = {age: 15, name:'Andr', city:'Moscow', authorized:true }
+const person2 = {age: 14, name:'Bori', city:'Uryupinsk', authorized:true }
+const person3 = {age: 10, name: 'Den', city:'Tver', authorized: false}
+
+const clients = [person1,person2,person3]
+
+
+
+console.table(clientsAuthorized)
