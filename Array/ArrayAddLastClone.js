bfcd49134cd04f06f84357cfbf590dcc11d3aac3
Berkashw <100141246+Berkashw@users.noreply.github.com>
2022-03-01 17:46:19 +0300

    add files

---
+++ Array/ArrayAddLastClone.js
@@ -0,0 +1,9 @@
+const person1 = {age: 15, name:'Andr', city:'Moscow', authorized:true }
+const person2 = {age: 14, name:'Bori', city:'Uryupinsk', authorized:false }
+const person3 = {age: 10, name: 'Den', city:'Tver', authorized: true}
+
+let clients = [person1,person2,person3]
+
+clients.unshift(clients[clients.length -1])
+
+console.table(clients)
