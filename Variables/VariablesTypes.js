d42a2c9458a7be757a0f48bf3beb9809834a524a
Berkashw <100141246+Berkashw@users.noreply.github.com>
2022-03-03 17:00:17 +0300

    add files

---
+++ Variables/VariablesTypes.js
@@ -0,0 +1,17 @@
+const numberType = 10
+const stringType = 'String Variable'
+const booleanType = true
+const nullType = null
+const undefinedType = undefined
+const objectType = {
+    number: 10,
+    string: 'abc',
+    boolean: true
+}
+
+console.log( numberType + ' - ' + typeof (numberType))
+console.log(stringType + ' - ' + typeof(stringType))
+console.log(booleanType + ' - ' + typeof(booleanType))
+console.log(nullType + ' - ' + typeof(nullType))
+console.log(undefinedType + ' - ' + typeof(undefinedType))
+console.log(objectType) 
