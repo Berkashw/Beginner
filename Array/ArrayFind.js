191bb141b5558553574a3b8f10e2eb7b94843e5f
Berkashw <100141246+Berkashw@users.noreply.github.com>
2022-03-04 09:21:10 +0300

    add files

--- Array/ArrayFind.js
+++ Array/ArrayFind.js
@@ -1,7 +1,12 @@
-const person1 = {age: 15, name:'Andr', city:'Moscow', authorized:false }
-const person2 = {age: 14, name:'Bori', city:'Uryupinsk', authorized:false }
-const person3 = {age: 10, name: 'Den', city:'Tver', authorized: true}
+const person1 = {age: 15, name:'Andr', city:'Moscow', isAuthorized:false }
+const person2 = {age: 14, name:'Bori', city:'Uryupinsk', isAuthorized:false }
+const person3 = {age: 10, name: 'Den', city:'Tver', isAuthorized: true}
 
 const clients = [person1,person2,person3]
 
-console.log(clients.find((a) => a.name == 'Den'  ))
+console.log(clients.find((person) => {
+const {name} = person
+const isEqual = name === 'Den'
+return isEqual
+}))
+console.log(clients.find(({name}) => name === 'Den'))
