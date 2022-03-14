adedd0007a2c54b983640bfca6efd036011bdb3e
Berkashw <100141246+Berkashw@users.noreply.github.com>
2022-03-14 17:27:46 +0300

    add files

---
+++ Array/ArrayWhile.js
@@ -0,0 +1,16 @@
+const person1 = {age: 15, name:'Andr', city:'Moscow', authorized:true }
+const person2 = {age: 14, name:'Bori', city:'Pskov', authorized:false }
+const person3 = {age: 10, name: 'Den', city:'Tver', authorized: true}
+const person4 = {age: 25, name:'Ed', city:'Moscow',authorized:false}
+const person5 = {age: 17, name:'Fin', city: 'Tula', authorized:true}
+const person6 = {age: 49, name:'Gor', city: 'Tver',authorized:false}
+
+const clients = [person1,person2,person3,person4,person5,person6]
+
+let i = 0
+while (i<clients.length) {
+    clients[i].age += clients[i].age
+    clients[i].older = true
+        i++
+}
+console.table(clients)
