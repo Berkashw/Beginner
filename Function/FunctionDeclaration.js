6e8de0270b478a8bb794faaab1b9bf986683a601
Berkashw <100141246+Berkashw@users.noreply.github.com>
2022-03-16 16:47:07 +0300

    add files

--- Function/FunctionDeclaration.js
+++ Function/FunctionDeclaration.js
@@ -2,8 +2,7 @@
 function sumNumbersFunction(number1,number2) {
     return number1 + number2
 }
+
 console.log('Funtion - ' + sumNumbersFunction(19,11))
 
-const sumNumbersArrowFuntion = (number1,number2) => number1 + number2
-console.log ('Arrow Function - ' + sumNumbersArrowFuntion(9,13))
 
