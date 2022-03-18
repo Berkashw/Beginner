ad3fc7150eaf99eca3c843d675af3c23ab8525c8
Berkashw <100141246+Berkashw@users.noreply.github.com>
2022-03-18 16:05:02 +0300

    add files

--- Function/FunctionDeclaration.js
+++ Function/FunctionDeclaration.js
@@ -1,8 +1,11 @@
 'use strict'
+
+console.log('Funtion before declaration - ' + sumNumbersFunction(19,11))
+
 function sumNumbersFunction(number1,number2) {
     return number1 + number2
 }
 
-console.log('Funtion - ' + sumNumbersFunction(19,11))
+console.log('Funtion after declaration - ' + sumNumbersFunction(19,11))
 
 
