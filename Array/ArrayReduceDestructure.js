273cb20eb3f86b60ca775db214a2c3091f118284
Berkashw <100141246+Berkashw@users.noreply.github.com>
2022-03-22 11:43:08 +0300

    add files

--- Array/ArrayReduceDestructure.js
+++ Array/ArrayReduceDestructure.js
@@ -7,8 +7,8 @@ const person6 = {age: 49, name:'Gor', city: 'Tver',authorized:false}
 
 
 const clients = [person1,person2,person3,person4,person5,person6]
-const acumAge = clients.reduce((sum, {age}) =>  {return sum + age},0)
-const acumAgeRight = clients.reduceRight((sum, {age}) =>  {return sum + age},0)
+const acumAge = clients.reduce((sum, {age}) =>  sum + age,0)
+const acumAgeRight = clients.reduceRight((sum, {age}) => sum + age,0)
 
 console.table(acumAge)
 console.table(acumAgeRight)
