e005504006b9fdc400b6376cc1814b29207044f3
Berkashw <100141246+Berkashw@users.noreply.github.com>
2022-03-22 15:31:59 +0300

    add files

--- Array/ArrayDouble.js
+++ Array/ArrayDouble.js
@@ -3,8 +3,10 @@ const person2 = {age: 14, name:'Bori', city:'Pskov', authorized:false }
 const person3 = {age: 10, name: 'Den', city:'Tver', authorized: true}
 
 const clients = [person1,person2,person3]
-
-const clientsDoubleAge = clients.map((client)=> client.age *= 2
-)
+const multiple = 2
+const clientsDoubleAge = clients.map((client)=> {
+    client.age *= multiple
+    return client
+})
 
 console.table(clientsDoubleAge)
