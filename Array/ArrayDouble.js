2eba2c9b56b46878bfc554d52854d66dcd5477b5
Berkashw <100141246+Berkashw@users.noreply.github.com>
2022-03-23 11:28:57 +0300

    update

--- Array/ArrayDouble.js
+++ Array/ArrayDouble.js
@@ -5,12 +5,15 @@ const person3 = {age: 10, name: 'Den', city:'Tver', authorized: true}
 const clients = [person1,person2,person3]
 
 function multiple(client,multIndex) { 
-    client.age*=multIndex
-    return client
+      client.age *= multIndex
+   return client
 }
 
-const clientsDoubleAge = clients.map((client) => multiple(client,1))
-
-const clientsTripleAge = clients.map((client) => multiple(client,3))
+const clientsDoubleAge = clients.map(({...client}) => multiple(client,2))
 console.table(clientsDoubleAge)
+
+const clientsTripleAge = clients.map(({...client}) => multiple(client,3))
+console.log (clientsDoubleAge)
 console.table(clientsTripleAge)
+
+console.log(clients)
