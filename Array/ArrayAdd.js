b8ec41370d2848880a36bd1f5bc42eb31c2ff840
Berkashw <100141246+Berkashw@users.noreply.github.com>
2022-03-25 16:00:17 +0300

    no ctlf, lf

--- Array/ArrayAdd.js
+++ Array/ArrayAdd.js
@@ -1,11 +1,11 @@
-const person1 = { age: 15, name: "Andr", city: "Moscow", authorized: true }
-const person2 = { age: 14, name: "Bori", city: "Uryupinsk", authorized: false }
-const person3 = { age: 10, name: "Den", city: "Tver", authorized: true }
+const person1 = { age: 15, name: 'Andr', city: 'Moscow', authorized: true }
+const person2 = { age: 14, name: 'Bori', city: 'Uryupinsk', authorized: false }
+const person3 = { age: 10, name: 'Den', city: 'Tver', authorized: true }
 
 let clients = [person1, person2, person3]
 
 clients.forEach((element) => {
-	element.available = true
+  element.available = true
 })
 
 console.table(clients)
