ea49cea4071d0fdd9de45757e5b2c0943e3af837
Berkashw <100141246+Berkashw@users.noreply.github.com>
2022-03-25 15:28:16 +0300

    no ctlf, lf

--- Array/ArrayMultiplier.js
+++ Array/ArrayMultiplier.js
@@ -1,28 +1,28 @@
-const person1 = { age: 15, name: "Andr", city: "Moscow", authorized: true }
-const person2 = { age: 14, name: "Bori", city: "Pskov", authorized: false }
-const person3 = { age: 10, name: "Den", city: "Tver", authorized: true }
-const person4 = { age: 25, name: "Ed", city: "Moscow", authorized: false }
-const person5 = { age: 17, name: "Fin", city: "Tula", authorized: true }
-const person6 = { age: 49, name: "Gor", city: "Tver", authorized: false }
+const person1 = { age: 15, name: 'Andr', city: 'Moscow', authorized: true }
+const person2 = { age: 14, name: 'Bori', city: 'Pskov', authorized: false }
+const person3 = { age: 10, name: 'Den', city: 'Tver', authorized: true }
+const person4 = { age: 25, name: 'Ed', city: 'Moscow', authorized: false }
+const person5 = { age: 17, name: 'Fin', city: 'Tula', authorized: true }
+const person6 = { age: 49, name: 'Gor', city: 'Tver', authorized: false }
 
 const clients = [person1, person2, person3, person4, person5, person6]
 
 const getArrayWhithMultipliedParameter = (
-	array,
-	multiplier,
-	parameter = "age"
+  array,
+  multiplier,
+  parameter = 'age'
 ) => {
-	const arrayResult = array.map(({ ...item }) => {
-		item[parameter] *= multiplier
-		return item
-	})
-	return arrayResult
+  const arrayResult = array.map(({ ...item }) => {
+    item[parameter] *= multiplier
+    return item
+  })
+  return arrayResult
 }
 
-const arrayMultiplied = getArrayWhithMultipliedParameter(clients, 2, "age")
+const arrayMultiplied = getArrayWhithMultipliedParameter(clients, 2, 'age')
 const arrayMultipliedWithoutParameter = getArrayWhithMultipliedParameter(
-	clients,
-	2
+  clients,
+  2
 )
 console.table(arrayMultiplied)
 console.table(arrayMultipliedWithoutParameter)
