ccdd0ae48d331fb328b4c6922548c1b50732da67
Berkashw <100141246+Berkashw@users.noreply.github.com>
2022-03-25 16:01:56 +0300

    no ctlf, lf

--- Class/AnimalClass.js
+++ Class/AnimalClass.js
@@ -1,33 +1,33 @@
 const dog = {
-	age: 1,
-	type: "dog",
-	voice: "Barkinq",
-	size: "medium",
-	color: "red",
-	name: "Red Barking Dog",
+  age: 1,
+  type: 'dog',
+  voice: 'Barkinq',
+  size: 'medium',
+  color: 'red',
+  name: 'Red Barking Dog',
 }
 const cat = {
-	age: 2,
-	type: "Cat",
-	voice: "Meow",
-	size: "Small",
-	color: "grey",
-	name: "Demon",
+  age: 2,
+  type: 'Cat',
+  voice: 'Meow',
+  size: 'Small',
+  color: 'grey',
+  name: 'Demon',
 }
 class Animal {
-	constructor(animalObject) {
-		this.type = animalObject.type
-		this.animalVoice = animalObject.voice
-		this.animalSize = animalObject.size
-		this.animalColor = animalObject.color
-		this.name = animalObject.name
-	}
-	saySomthing = () => {
-		console.log(this.animalVoice)
-	}
-	sayHello = () => {
-		console.log("hi " + this.name)
-	}
+  constructor(animalObject) {
+    this.type = animalObject.type
+    this.animalVoice = animalObject.voice
+    this.animalSize = animalObject.size
+    this.animalColor = animalObject.color
+    this.name = animalObject.name
+  }
+  saySomthing = () => {
+    console.log(this.animalVoice)
+  }
+  sayHello = () => {
+    console.log('hi ' + this.name)
+  }
 }
 const cat1 = new Animal(cat)
 const dog1 = new Animal(dog)
